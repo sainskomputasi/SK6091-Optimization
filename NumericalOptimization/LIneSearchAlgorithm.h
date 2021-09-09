bea67dd2e7145433c9f5c56e4005b3f8ad768603
Michael Sipayung <michaelsipayung123@gmail.com>
2021-09-09 17:27:23 +0700

    add interpolation function

--- NumericalOptimization/LIneSearchAlgorithm.h
+++ NumericalOptimization/LIneSearchAlgorithm.h
@@ -5,11 +5,17 @@
 namespace MA5171 {
 	class Optimization {
 	public:
-		inline double f(double);
-		inline double gradient(double);
+		inline double f(double,double);
+		inline double fak_x1(double,double);
+		inline double fak_x2(double, double);
 		inline std::ofstream& write(std::ofstream&, std::string);
-		inline double zoom(double, double);
+		inline double zoom(double a_lo, double a_hi, double phi_lo, double phi_hi, double derphi_lo, double phi(double),  
+			double derphi(double), double phi0, double derphi0, double c1, double c2);
 		double LineSearch(double, double, double);
+		double cubicInter(double , double , double , double , double , double , double );
+		double quadInter(double , double , double , double , double );
+
+
 	private:
 	};
 }
--- NumericalOptimization/NumericalOptimization.cpp
+++ NumericalOptimization/NumericalOptimization.cpp
@@ -1,50 +1,100 @@
 ﻿#include "NumericalOptimization.h"
 #include "LIneSearchAlgorithm.h"
+#include <Eigen/dense>
 #include <vector>
 int main()
 {
+	MA5171::Optimization test;
 	
 	return 0;
 }
-inline double MA5171::Optimization::f(double x) {
-	return std::pow((-5.0 + x), 2.0);
+inline double MA5171::Optimization::f(double x1,double x2) {
+	return (1 + ((x1 + x2) / 4000) - (std::cos(x1) * std::cos((0.5) * x2 * std::sqrt(2.0))));
 }
-inline double MA5171::Optimization::gradient(double x) {
-	return (2.0 * (-5.0 + x));
+inline double MA5171::Optimization::fak_x1(double x1,double x2) {
+	return ((x1 / 2000) + 
+		(std::sin(x1) * std::cos(x2 * std::sqrt(2.0) / 2.0)));
+}
+inline double MA5171::Optimization::fak_x2(double x1, double x2) {
+	return ((x2 / 2000) +
+		(std::cos(x1) * std::sqrt(2.0) * std::sin(x2 * std::sqrt(2.0) / 2.0)) / 2.0);
 }
 inline std::ofstream &MA5171::Optimization::write(std::ofstream& name,std::string fileName) {
 	name.open(fileName, std::ios::app);
 	return name;
 }
-inline double MA5171::Optimization::zoom(double alphaLo, double alphaHi) {
-	double c1 = 0.0;
-	double c2 = 0.0;
-	double aStar = 0.0;
-	int begin = 1;
-	while (begin)
+inline double MA5171::Optimization::zoom(double a_lo, double a_hi, double phi_lo, double phi_hi, double derphi_lo, double phi(double),
+	double derphi(double), double phi0, double derphi0, double c1, double c2) {
+	int begin = 0;
+	int max = 11;
+	double setFirstDel = 0.2;
+	double setSectDel = 0.1; //parameter set interp ...
+	double phi_rec = phi0;
+	double arec = 0;
+	double a, b = 0;
+	double cchk,aj = 0.0; //for cub ..
+	double qchk = 0.0;//for quad...
+	double a_star = 0.0;
+	double dAlpha = 0.0;
+	while (begin!=max)
 	{
-		double aj = 0;//return interpolate result	
-		if (f(aj) > f(0) + c1 * aj * gradient(0) || f(aj) >= f(alphaLo)) {
-			alphaHi = aj;
+		dAlpha = a_hi - a_lo;
+		if (dAlpha<0)
+		{
+			a, b = a_hi, a_lo;
 		}
 		else {
-			if (std::fabs(gradient(aj)) <= (-c2) * gradient(0))
+			a, b = a_lo, a_hi;
+		}
+		if (begin > 0) {
+			cchk = setFirstDel * dAlpha;
+			aj = cubicInter(a_lo, phi_lo, derphi_lo, a_hi, phi_hi, arec, phi_rec);
+		}
+		if ((begin==0)||(aj>b-cchk)||(aj<a+cchk))
+		{
+			qchk = setSectDel * dAlpha;
+			aj = quadInter(a_lo, phi_lo, derphi_lo, a_hi, phi_hi);
+			if ((aj==NULL)||(aj>b-qchk)||(aj<a+qchk))
 			{
-				aStar = aj;
+				aj = a_lo + 0.5 * dAlpha;//use bisec
+			}
+		}
+		double phi_aj = phi(aj);
+		//test after interpolate
+		if ((phi_aj>phi0+c1*aj*derphi0)||(phi_aj>=phi_lo))
+		{
+			phi_rec = phi_hi;
+			arec = a_hi;
+			a_hi = aj;
+			phi_hi = phi_aj;
+		}
+		else {
+			double derphi_aj = derphi(aj);
+			if (std::fabs(derphi_aj)<=-c2*derphi0)
+			{
+				a_star = aj;
 				break;
 			}
-			if ((gradient(aj)*(alphaHi-alphaLo))>=0)
-			{
-				alphaHi = alphaLo;
-
+			if ((derphi_aj * (a_hi - a_lo))>=0) {
+				phi_rec = phi_hi;
+				arec = a_hi;
+				a_hi = a_lo;
+				phi_hi = phi_lo;
 			}
-			alphaLo = aj;
+			else {
+				phi_rec = phi_lo;
+				arec = a_lo;
+			}
+			a_lo = aj;
+			phi_lo = phi_aj;
+			derphi_lo = derphi_aj;
 		}
+
+		++begin;
 	}
-	
-	return aStar;
+	return a_star;
 }
-double MA5171::Optimization::LineSearch(double alpha0, double alphaMax, double alpha1) {
+/*double MA5171::Optimization::LineSearch(double alpha0, double alphaMax, double alpha1) {
 	std::vector<double>alpha(10);
 	double c1 = 0.0, c2 = 0.0;
 	double astar = 0.0;
@@ -54,15 +104,15 @@ double MA5171::Optimization::LineSearch(double alpha0, double alphaMax, double a
 	}
 	int i = 1;
 	while (i) {
-		if (f(alpha[i]) > f(0) + c1 * alpha[i] * gradient(0) || (f(alpha[i]) >= f(alpha[i - 1]) && i > 1)) {
+		if (f(alpha[i], alpha[i]) > f(0.0,0.0) + c1 * alpha[i] * fak_x1(0.0,0.0) || (f(alpha[i], alpha[i]) >= f(alpha[i - 1],alpha[i-1]) && i > 1)) {
 			astar = zoom(alpha[i - 1], alpha[i]);
 			break;
 		}
-		if (fabs(gradient(alpha[i])) <= -c2 * gradient(0)) {
+		if (fabs(fak_x1(alpha[i], alpha[i])) <= -c2 * fak_x1(0.0,0.0)) {
 			astar = alpha[i];
 			break;
 		}
-		if (gradient(alpha[i])>=0.0)
+		if (fak_x1(alpha[i], alpha[i])>=0.0)
 		{
 			astar = zoom(alpha[i], alpha[i - 1]);
 			break;
@@ -70,4 +120,32 @@ double MA5171::Optimization::LineSearch(double alpha0, double alphaMax, double a
 		++i;
 	}
 	return astar;
+}*/
+
+double MA5171::Optimization::cubicInter(double a, double fa, double fpa, double b, double fb, double c, double fc) {
+	double C = fpa;
+	double db = b - a;
+	double dc = c - a;
+	double denomin = std::pow((db * dc), 2.0) * (db - dc);
+	//call eigen matrix template.. intended for solving the equation 
+	Eigen::Matrix2d d1;
+	d1 << std::pow(dc, 2.0), std::pow(-db, 2.0), 
+		  std::pow(-dc, 3.0), std::pow(db, 3.0);
+	Eigen::Matrix<double, 2, 1> multipl;
+	multipl << (fb - fa - C * db), (fc - fa - C * dc);
+	Eigen::Matrix<double, 2, 1> tempor;
+	tempor << d1 * multipl;
+	tempor(0, 0) /= denomin;
+	tempor(1, 0) /= denomin;
+	double rad = std::pow(tempor(1,0),2.0) - 3 * tempor(0, 0)*C;
+	double xmin = a + (-tempor(1, 0) + std::sqrt(rad)) / (3 * tempor(0,0));
+	return xmin;
+}
+double MA5171::Optimization::quadInter(double a, double fa, double fpa, double b, double fb) {
+	double D = fa; 
+	double C = fpa;
+	double db = b - a * 1.0;
+	double B = (fb - D - C * db) / (std::pow(db,2.0));
+	double xmin = a - C / (2.0 * B); //solve for quad interp ...
+	return xmin;
 }
